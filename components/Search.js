a81dac8778206182c025bb56e6f58fa01175f6e0
Maja Jelenik <maja.jelenik@vp.pl>
2018-07-18 23:34:37 +0200

    Change search.js file

--- components/Search.js
+++ components/Search.js
@@ -1,85 +1,40 @@
 Search = React.createClass({
-    
-    getInitialState(){
+    getInitialState() {
         return {
-            searchingText: ''
+        searchingText: ''
         };
     },
-    
+
     handleChange: function(event) {
         var searchingText = event.target.value;
-        this.setState({
-            searchingText: searchingText
-        });
+        this.setState({searchingText: searchingText});
+
         if (searchingText.length > 2) {
-        this.props.onSearch(searchingText);
+          this.props.onSearch(searchingText);
+        }
+    },
+    handleKeyUp: function(event) {
+        if (event.keyCode === 13) {
+          this.props.onSearch(this.state.searchingText);
         }
     },
 
-    handleKeyUp: function(event) {
-        if (event.keyCode === 13) {
-            this.props.onSearch(this.state.searchingText);
-        }
-    },
-    
     render: function() {
-        var styles = {
+            
+            var styles = {
             fontSize: '1.5em',
             width: '90%',
             maxWidth: '350px'
         };
-        
-    return <input
+
+        return <input
             type="text"
             onChange={this.handleChange}
+            onKeyUp={this.handleKeyUp}
             placeholder="Tutaj wpisz wyszukiwaną frazę"
             style={styles}
             value={this.state.searchTerm}
-            />
+        />
     }
-
-});
-   
-    <Search onSearch={this.handleSearch}/>
-                           
-render: function() {
-    var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
     
-    return <input 
-                type="text"
-                onChange={this.handleKeyUp}
-                onKeyUp={this.handleKeyUp}
-                placeholder="Tutaj wpisz wyszukiwaną frazę"
-                style={styles}
-                value={this.state.searchTerm}
-            />
-}
-handleSearch: function(searchingText) {  // 1.
-    this.setState({
-      loading: true  // 2.
-    });
-    this.getGif(searchingText, function(gif) {  // 3.
-      this.setState({  // 4
-        loading: false,  // a
-        gif: gif,  // b
-        searchingText: searchingText  // c
-      });
-    }.bind(this));
-  },
-      
-getGif: function(searchingText, callback) {  // 1.
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-    var xhr = new XMLHttpRequest();  // 3.
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-           var data = JSON.parse(xhr.responseText).data; // 4.
-            var gif = {  // 5.
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-            };
-            callback(gif);  // 6.
-        }
-    };
-    xhr.send();
-},
+});
